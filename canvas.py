bf35fdf5e6a1654015b83f88a7aecb55e615850a
QwiglyDee <qwiglydee@gmail.com>
2026-06-01 19:31:09 +0700

    cleanup canvas coords

--- canvas.py
+++ canvas.py
@@ -9,36 +9,21 @@ from palette import pick_color
 
 CANVAS_SIZE = 640
 PADDING = 4
-CELL_SIZE = (640 - PADDING * 2) / 9
-SEGM_SIZE = CELL_SIZE / 3
-FONT1_SIZE = 12
-FONT1 = f"{FONT1_SIZE}px sans-serif"
-FONT2_SIZE = 32
-FONT2 = f"{FONT2_SIZE}px sans-serif"
-FONT_COLORS = {"": "#000000", "HIGH": "#FFFFFF", "FINAL": "#606060"}
+FONT_SIZE = 12
+FONT = f"{FONT_SIZE}px sans-serif"
+FONT_COLORS = {"": "#000000", "HIGH": "#FFFFFF", "FINAL": "#404040"}
 BG_COLOR = "#808080"
 
-HIGHLIGHT_SIZE = CELL_SIZE / 3
-HIGHLIGHT_R = HIGHLIGHT_SIZE / 2 - 2
 
-WIDTHS = {
-    "HARD": 6,
-    "SOFT": 6,
-    "SOLID": 6,
-    "GROUP": SEGM_SIZE - 2,
-}
-
-DASHES = {
-    "SOLID": [],
-    "HARD": [],
-    "SOFT": [3, 3],
-}
-
-
-HIGHLIGHT_COLOR = pick_color("blue")
+def segm_rc(seg: int):
+    d0 = seg - 1
+    return d0 // 3, d0 % 3
 
 
 class XY(NamedTuple):
+    CELL = (640 - PADDING * 2) / 9
+    SEGM = CELL / 3
+
     x: float
     y: float
 
@@ -55,44 +40,61 @@ class XY(NamedTuple):
         v = XY(p2.x - p1.x, p2.y - p1.y)
         return math.sqrt(v.x * v.x + v.y * v.y)
 
+    @classmethod
+    def ofcell(cls, loc: Loc) -> Self:
+        """top-left coords of a cell"""
+        return cls((loc.col - 1) * cls.CELL, (loc.row - 1) * cls.CELL)
+
+    def loc(self) -> Loc:
+        """map the xy to a cell"""
+        return Loc(int(self.y / self.CELL) + 1, int(self.x / self.CELL) + 1)
+
+    @classmethod
+    def ofsect(cls, loc1: Loc, loc2: Loc) -> tuple[Self, Self]:
+        """top-left and bot-right"""
+        p1 = cls.ofcell(loc1)
+        p2 = cls.ofcell(loc2)
+        return p1, cls(p2.x + cls.CELL, p2.y + cls.CELL)
+
+    @classmethod
+    def ofsegm(cls, loc: Loc, seg: int) -> Self:
+        """center of a segment"""
+        r, c = segm_rc(seg)
+        x0, y0 = cls.ofcell(loc)
+        return cls(x0 + (c + 0.5) * cls.SEGM, y0 + (r + 0.5) * cls.SEGM)
+
+    def locseg(self) -> tuple[Loc, int]:
+        """map the xy to a cell + segm"""
+        loc = self.loc()
+        scol = int(self.x / self.SEGM)
+        srow = int(self.y / self.SEGM)
+        return loc, 1 + srow * 3 + scol
+
+
+HIGHLIGHT_SIZE = XY.CELL / 3
+HIGHLIGHT_R = HIGHLIGHT_SIZE / 2 - 2
+
+WIDTHS = {
+    "HARD": 6,
+    "SOFT": 6,
+    "SOLID": 6,
+    "GROUP": XY.SEGM - 2,
+}
+
+DASHES = {
+    "SOLID": [],
+    "HARD": [],
+    "SOFT": [3, 3],
+}
+
+
+HIGHLIGHT_COLOR = pick_color("blue")
+
 
 XYs = tuple[XY, ...]
 
 
 P0 = XY(PADDING, PADDING)
-
-
-def cell_xy(loc: Loc):
-    """top-bot coords of a cell"""
-    return XY((loc.col - 1) * CELL_SIZE, (loc.row - 1) * CELL_SIZE)
-
-
-def xy_cell(p: XY) -> Loc:
-    return Loc(int(p.y / CELL_SIZE) + 1, int(p.x / CELL_SIZE) + 1)
-
-
-def segm_rc(seg: int):
-    """sub-loc of a segment in a cell"""
-    d0 = seg - 1
-    return d0 // 3, d0 % 3
-
-
-def segm_xy(seg: int):
-    """coords of a segment in a cell"""
-    r, c = segm_rc(seg)
-    return XY((c + 0.5) * SEGM_SIZE, (r + 0.5) * SEGM_SIZE)
-
-
-def xy_segm(p: XY) -> int:
-    c = int(p.x / SEGM_SIZE)
-    r = int(p.y / SEGM_SIZE)
-    return 1 + r * 3 + c
-
-
-def targ_xy(loc: Loc, seg: int):
-    c = cell_xy(loc)
-    s = segm_xy(seg)
-    return XY(c.x + s.x, c.y + s.y)
 
 
 class SudokuCanvas(MultiCanvas):
@@ -110,7 +112,7 @@ class SudokuCanvas(MultiCanvas):
         digits_cnv.text_align = "center"
 
         hlight_cnv = self[self.HLIGHT_LAYER]
-        hlight_cnv.font = FONT1
+        hlight_cnv.font = FONT
         hlight_cnv.text_baseline = "middle"
         hlight_cnv.text_align = "center"
         hlight_cnv.global_alpha = 0.625
@@ -119,53 +121,45 @@ class SudokuCanvas(MultiCanvas):
 
     def draw_grid(self):
         canvas = self[self.GRID_LAYER]
-        size = CELL_SIZE * 9
+        fullsize = XY.CELL * 9
 
         canvas.clear()
         with hold_canvas():
             canvas.fill_style = BG_COLOR
-            canvas.fill_rect(P0.x, P0.y, size, size)
+            canvas.fill_rect(P0.x, P0.y, fullsize, fullsize)
 
             canvas.stroke_style = "rgba(0, 0, 0, 0.25)"
             for i in range(1, 9):
-                c9 = XY(i * CELL_SIZE, i * CELL_SIZE)
-                canvas.stroke_line(P0.x, P0.x + c9.y, P0.x + size, P0.y + c9.y)
-                canvas.stroke_line(P0.x + c9.x, P0.y, P0.x + c9.x, P0.y + size)
+                p9 = XY.add(P0, XY(i * XY.CELL, i * XY.CELL))
+                canvas.stroke_line(P0.x, p9.y, P0.x + fullsize, p9.y)
+                canvas.stroke_line(p9.x, P0.y, p9.x, P0.y + fullsize)
             canvas.stroke_style = "rgba(0, 0, 0, 0.5)"
             for i in range(1, 3):
-                c3 = XY(i * CELL_SIZE * 3, i * CELL_SIZE * 3)
-                canvas.stroke_line(P0.x, P0.y + c3.y, P0.x + size, P0.y + c3.y)
-                canvas.stroke_line(P0.x + c3.x, P0.y, P0.x + c3.x, P0.y + size)
-            canvas.line_width = 3
+                p3 = XY.add(P0, XY(i * XY.CELL * 3, i * XY.CELL * 3))
+                canvas.stroke_line(P0.x, p3.y, P0.x + fullsize, p3.y)
+                canvas.stroke_line(p3.x, P0.y, p3.x, P0.y + fullsize)
+            canvas.line_width = 4
             canvas.stroke_style = "rgba(0, 0, 0, 1.0)"
-            canvas.stroke_rect(P0.x, P0.y, size, size)
+            canvas.stroke_rect(P0.x - 2, P0.y - 2, fullsize + 2, fullsize + 2)
 
     def draw_board(self, board: Board, highlight: set[int] = set()):
         canvas = self[self.DIGITS_LAYER]
-
-        def digcolor(dig: int):
-            return "HIGH" if dig in highlight else ""
 
         canvas.clear()
         with hold_canvas():
             for node in board:
                 if node.cell.is_empty:
                     continue
-
-                c = cell_xy(node.loc)
-                if node.cell.final is not None:
-                    dig = node.cell.final
-                    canvas.font = FONT2
-                    canvas.fill_style = FONT_COLORS["FINAL"]
-                    canvas.fill_text(str(dig), P0.x + c.x + 0.5 * CELL_SIZE, P0.x + c.y + 0.5 * CELL_SIZE)
-                else:
-                    for dig in range(1, 10):
-                        if dig not in node.cell:
-                            continue
-                        s = segm_xy(dig)
-                        canvas.font = FONT1
-                        canvas.fill_style = FONT_COLORS[digcolor(dig)]
-                        canvas.fill_text(str(dig), P0.x + c.x + s.x, P0.y + c.y + s.y)
+                for dig in node.cell:
+                    p = XY.add(P0, XY.ofsegm(node.loc, dig))
+                    canvas.font = FONT
+                    if node.cell.is_final:
+                        canvas.fill_style = FONT_COLORS["FINAL"]
+                    elif dig in highlight:
+                        canvas.fill_style = FONT_COLORS["HIGH"]
+                    else:
+                        canvas.fill_style = FONT_COLORS[""]
+                    canvas.fill_text(str(dig), p.x, p.y)
 
     def clear_highlights(self):
         canvas = self[self.HLIGHT_LAYER]
@@ -173,33 +167,32 @@ class SudokuCanvas(MultiCanvas):
 
     def highlight_segment(self, loc: Loc, seg: int, *, color: str = HIGHLIGHT_COLOR):
         canvas = self[self.HLIGHT_LAYER]
-        c = cell_xy(loc)
-        s = segm_xy(seg)
+        p = XY.ofsegm(loc, seg)
         canvas.set_line_dash([])
         canvas.fill_style = pick_color(color)
-        canvas.clear_rect(P0.x + c.x + s.x - HIGHLIGHT_R, P0.y + c.y + s.y - HIGHLIGHT_R, HIGHLIGHT_SIZE, HIGHLIGHT_SIZE)
-        canvas.fill_circle(P0.x + c.x + s.x, P0.y + c.y + s.y, HIGHLIGHT_R)
+        canvas.clear_rect(P0.x + p.x - HIGHLIGHT_R, P0.y + p.y - HIGHLIGHT_R, HIGHLIGHT_SIZE, HIGHLIGHT_SIZE)
+        canvas.fill_circle(P0.x + p.x, P0.y + p.y, HIGHLIGHT_R)
 
     def highlight_link(self, loc1: Loc, seg1: int, loc2: Loc, seg2: int, *, color: str = HIGHLIGHT_COLOR, style: str = "SOLID"):
         canvas = self[self.HLIGHT_LAYER]
-        t1 = XY.add(P0, targ_xy(loc1, seg1))
-        t2 = XY.add(P0, targ_xy(loc2, seg2))
+        p1 = XY.add(P0, XY.ofsegm(loc1, seg1))
+        p2 = XY.add(P0, XY.ofsegm(loc2, seg2))
         canvas.set_line_dash(DASHES[style])
         canvas.line_width = WIDTHS[style]
         canvas.stroke_style = pick_color(color)
 
-        length = max(1, math.ceil(XY.dist(t1, t2) / CELL_SIZE))
+        length = max(1, math.ceil(XY.dist(p1, p2) / XY.CELL))
 
         if style == "HARD":
-            points = tuple(split_line(t1, t2, n=1 + length // 2))
-            points = tuple(jig_line(points, SEGM_SIZE))
+            points = tuple(split_line(p1, p2, n=1 + length // 2))
+            points = tuple(jig_line(points, XY.SEGM))
             stroke_quadsmooth_path(canvas, points)
         elif style == "SOFT":
-            points = tuple(split_line(t1, t2, n=2 * length))
-            points = tuple(jig_line(points, SEGM_SIZE))
+            points = tuple(split_line(p1, p2, n=2 * length))
+            points = tuple(jig_line(points, XY.SEGM))
             stroke_quadsmooth_path(canvas, points)
         else:
-            canvas.stroke_line(t1.x, t1.y, t2.x, t2.y)
+            canvas.stroke_line(p1.x, p1.y, p2.x, p2.y)
 
 
 def split_line(p1: XY, p2: XY, n: int) -> Iterable[XY]:
