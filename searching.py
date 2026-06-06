8f12a63ab5aca8472fda819526ec97bf4df885fc
QwiglyDee <qwiglydee@gmail.com>
2026-06-06 10:16:48 +0700

    fact out searching

---
+++ searching.py
@@ -0,0 +1,30 @@
+from collections import deque
+from collections.abc import Generator
+from typing import Callable, Iterable, TypeVar
+
+
+T = TypeVar("T")
+
+
+def search_breadth[T](init: Iterable[T], expand: Callable[[T], Iterable[T]], criteria: Callable[[T], bool]) -> Generator[T]:
+    """Generic infinite bread-first search"""
+    frontier = deque[T](init)  # queue
+    explored = set[T]()
+    while frontier:
+        state = frontier.popleft()
+        if criteria(state):
+            yield state
+        explored.add(state)
+        frontier.extend(ext for ext in expand(state) if ext not in explored and ext not in frontier)
+
+
+def search_depth[T](init: Iterable[T], expand: Callable[[T], Iterable[T]], criteria: Callable[[T], bool]) -> Generator[T]:
+    """Generic infinite bread-first search"""
+    frontier = deque[T](init)  # stack
+    explored = set[T]()
+    while frontier:
+        state = frontier.pop()
+        if criteria(state):
+            yield state
+        explored.add(state)
+        frontier.extend(ext for ext in expand(state) if ext not in explored and ext not in frontier)
